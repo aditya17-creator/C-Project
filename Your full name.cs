48c6234e1b17a67cf4f5af35f00102607493e7c0
aditya17-creator <55273418+aditya17-creator@users.noreply.github.com>
2022-06-09 13:49:26 +0530

    Add files via upload

---
+++ Your full name.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string fname,lname;
+Console.Write("Enter your first name : ");
+fname = Console.ReadLine();
+Console.Write("Enter your last name : ");
+lname = Console.ReadLine();
+Console.Write("Your full name is "+fname+" "+lname);
+
+
